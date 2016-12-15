12ae26dd102e320224384876e49f3c881e7fa1c8
Logan Pugh <logan.pugh@austintexas.gov>
2016-12-15 17:05:57 -0600

    Add ability to select which instances to publish to on publish dialog

--- src/publishdialog/__init__.py
+++ src/publishdialog/__init__.py
@@ -2,7 +2,7 @@ from PyQt4 import QtGui
 from PyQt4.QtCore import Qt
 
 from ags_service_publisher.logging_io import setup_logger
-from ags_service_publisher.publisher import get_configs, normalize_services
+from ags_service_publisher.publisher import get_config, get_configs, normalize_services
 from publishdialog import Ui_PublishDialog
 
 log = setup_logger(__name__)
@@ -12,10 +12,14 @@ class PublishDialog(QtGui.QDialog, Ui_PublishDialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
+
+        self.servicesTree.header().setResizeMode(0, QtGui.QHeaderView.Stretch)
+        self.instancesTree.header().setResizeMode(0, QtGui.QHeaderView.Stretch)
+
         for config_name, config in get_configs().iteritems():
             services = config.get('services')
             default_service_properties = config.get('default_service_properties')
-            config_item = QtGui.QTreeWidgetItem(self.treeWidget)
+            config_item = QtGui.QTreeWidgetItem(self.servicesTree)
             config_item.setText(0, config_name)
             config_item.setText(1, 'Config Name')
             config_item.setFlags(config_item.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
@@ -25,14 +29,29 @@ class PublishDialog(QtGui.QDialog, Ui_PublishDialog):
                 service_item.setText(1, '{} Service'.format(service_type))
                 service_item.setFlags(service_item.flags() | Qt.ItemIsUserCheckable)
                 service_item.setCheckState(0, Qt.Unchecked)
+        user_config = get_config('userconfig')
+        for env_name, env in user_config['environments'].iteritems():
+            env_item = QtGui.QTreeWidgetItem(self.instancesTree)
+            env_item.setText(0, env_name)
+            env_item.setText(1, 'Environment')
+            env_item.setFlags(env_item.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+            for instance_name in env.get('ags_instances'):
+                instance_item = QtGui.QTreeWidgetItem(env_item)
+                instance_item.setText(0, instance_name)
+                instance_item.setText(1, 'AGS Instance')
+                instance_item.setFlags(instance_item.flags() | Qt.ItemIsUserCheckable)
+                instance_item.setCheckState(0, Qt.Unchecked)
+
         self.buttonBox.accepted.connect(self.publish_selected_items)
 
     def publish_selected_items(self):
         included_configs = []
         included_services = []
-        root = self.treeWidget.invisibleRootItem()
-        for i in range(root.childCount()):
-            config_item = root.child(i)
+        included_envs = []
+        included_instances = []
+        services_root = self.servicesTree.invisibleRootItem()
+        for i in range(services_root.childCount()):
+            config_item = services_root.child(i)
             if config_item.checkState(0) in (Qt.Checked, Qt.PartiallyChecked):
                 config_name = str(config_item.text(0))
                 included_configs.append(config_name)
@@ -43,5 +62,25 @@ class PublishDialog(QtGui.QDialog, Ui_PublishDialog):
                     service_name = str(service_item.text(0))
                     included_services.append(service_name)
                     log.debug('Selected service name: {}'.format(service_name))
-        if len(included_configs) > 0:
-            self.parent().publish_services(included_configs=included_configs, included_services=included_services)
+        instances_root = self.instancesTree.invisibleRootItem()
+        for i in range(instances_root.childCount()):
+            env_item = instances_root.child(i)
+            if env_item.checkState(0) in (Qt.Checked, Qt.PartiallyChecked):
+                env_name = str(env_item.text(0))
+                included_envs.append(env_name)
+                log.debug('Selected env name: {}'.format(env_name))
+            for j in range(env_item.childCount()):
+                instance_item = env_item.child(j)
+                if instance_item.checkState(0) == Qt.Checked:
+                    instance_name = str(instance_item.text(0))
+                    included_instances.append(instance_name)
+                    log.debug('Selected instance name: {}'.format(instance_name))
+        if len(included_configs) > 0 and len(included_instances) > 0:
+            self.parent().publish_services(
+                included_configs=included_configs,
+                included_services=included_services,
+                included_envs=included_envs,
+                included_instances=included_instances
+            )
+        else:
+            log.warn('No publishable configs or instances were selected!')
