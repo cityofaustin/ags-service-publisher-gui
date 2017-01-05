9c3086a01f21550e52788635e1a4245bbeeb3e42
Logan Pugh <logan.pugh@austintexas.gov>
2017-01-05 14:55:06 -0600

    Fix SIP API error in frozen executable

--- build.bat
+++ build.bat
@@ -10,6 +10,7 @@ pyinstaller ^
     --exclude-module arcpy ^
     --runtime-hook .\hooks\rthooks\pyi_rth_multiprocessing.py ^
     --runtime-hook .\hooks\rthooks\pyi_rth_arcpy.py ^
+    --runtime-hook .\hooks\rthooks\pyi_rth_pyqt4.py ^
     --clean ^
     --noconsole ^
     -n ags_service_publisher_gui ^
