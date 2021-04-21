9ed1a3d1771476dd6a2335fc9bccdbfa5c498b46
Logan Pugh <logan.pugh@austintexas.gov>
2021-04-21 15:27:49 -0500

    Add optional code signing step to build script
      - Set environment variables to perform code signing:
        - AGSSPGUICERTSHA1: The SHA1 hash of the code signing certificate in the Windows certificate store
        - AGSSPGUITIMESTAMPURL: The timestamping URL to sign the executable with, e.g. http://timestamp.digicert.com

--- build.bat
+++ build.bat
@@ -31,3 +31,17 @@ pyinstaller ^
     --specpath %~dp0 ^
     -n ags_service_publisher_gui_pro ^
     %~dp0src\main.pyw
+
+if %ErrorLevel% equ 0 (
+    if defined AGSSPGUICERTSHA1 if defined AGSSPGUITIMESTAMPURL (
+        signtool ^
+            sign ^
+            /debug ^
+            /sm ^
+            /sha1 %AGSSPGUICERTSHA1% ^
+            /fd SHA256 ^
+            /tr %AGSSPGUITIMESTAMPURL% ^
+            /td SHA256 ^
+            %~dp0dist\ags_service_publisher_gui_pro.exe
+    )
+)
